0e19438fdbbdfc1dada72bd24a262e62291458c4
paulstafki <waynesworldwails@hotmail.com>
2015-10-25 22:24:30 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <p>
+      <?php
+        $items = 8;
+
+        if($items > 5) {
+          echo "You get a 10% discount!";
+
+        }
+      ?>
+    </p>
+  </body>
+</html>
