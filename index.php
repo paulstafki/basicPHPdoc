f2741f441d4f98167538bddcd83a617c80f688f8
paulstafki <waynesworldwails@hotmail.com>
2015-10-27 10:00:23 -0500

    playing with for-loop syntax, and echo/print using a p-tag

--- index.php
+++ index.php
@@ -8,7 +8,10 @@
 
         if($items > 5) {
           echo "You get a 10% discount!";
+        }
 
+        for ($leap = 2004; $leap < 2050; $leap = $leap + 4) {
+                echo "<p>$leap</p>";
         }
       ?>
     </p>
