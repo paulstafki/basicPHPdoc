d0cf01ce3fddc6d13df7b27d79280a54af82d922
paulstafki <waynesworldwails@hotmail.com>
2015-10-28 19:34:01 -0500

    A few more things of note, comments

--- index.php
+++ index.php
@@ -4,15 +4,19 @@
   <body>
     <p>
       <?php
-        $items = 8;
+        $items = 8;     // declaring a variable, array, object, etc
 
         if($items > 5) {
-          echo "You get a 10% discount!";
+          echo "You get a 10% discount!";       //basic conditional echo is like console.log, print puts it on the DOM
         }
 
-        for ($leap = 2004; $leap < 2050; $leap = $leap + 4) {
+        for ($leap = 2004; $leap < 2050; $leap = $leap + 4) {       //for loop
                 echo "<p>$leap</p>";
         }
+        print substr("Paul", (rand(0, strlen("Paul")-1)), 1);       //combination of string functions
+
+
+
       ?>
     </p>
   </body>
